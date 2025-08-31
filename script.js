48286bae6d167adf353c571a3a731f79645b6f49
Mateusz Śliwa <mateuszsliwa@Mateuszs-MacBook-Pro.local>
2025-08-31 21:22:04 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Płynne przewijanie do sekcji
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+  
